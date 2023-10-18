fb0d1acda79990680fc94b1b49bb40c77492d625
jaime0506 <jaimemejia1003@gmail.com>
2023-10-17 22:22:08 -0500

    create folder components and Clocl component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,7 @@
-import { useEffect, useState } from "react"
-
-// Tengo que pasarle el tipo de dato que va a a contener el optionsFormatDate
-// porque no lo reconoce si lo paso sin tipo al toLocaleTimeString
-
-const optionsFormatDate: Intl.DateTimeFormatOptions = {
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    hour12: true,
-    // Esto hará que el formato sea de 12 horas
-}
+import { Clock } from "./components"
 
 export const App = () => {
-    const [time, setTime] = useState<string>()
-
-    useEffect(() => {
-        const updateTime = () => {
-            const date = new Date()
-            const dateLocal = date.toLocaleTimeString('en-US', optionsFormatDate);
-
-            setTime(dateLocal)
-        }
-
-        updateTime()
-
-        const interval = setInterval(() => {
-            updateTime();
-        }, 1000)
-
-        return () => clearInterval(interval)
-    }, [])
-
     return (
-        <div className="bg-black h-screen flex justify-center items-center">
-            <h1 className="text-white text-8xl" style={{ fontFamily: "Share Tech Mono" }}>{time}</h1>
-        </div>
+        <Clock />
     )
 }
