9c776113f73cfd1397ad5a09e045de21a7f8fbb3
jaime0506 <jaimemejia1003@gmail.com>
2023-10-21 19:52:42 -0500

    add open and close modal animation

--- src/components/modal/ModalWrapper.tsx
+++ src/components/modal/ModalWrapper.tsx
@@ -12,18 +12,22 @@ export const ModalWrapper = ({ children }: Props) => {
 
     const { modal, handleCloseModal } = useUiStore()
 
-    if (!modal.isOpen) return null
-
     return (
         <AnimatePresence>
-            <motion.section
-                className="absolute top-0 flex bg-gray-950 bg-opacity-50 w-screen h-screen flex-col justify-center items-center"
-                onClick={handleCloseModal}
-            >
-                <Modal >
-                    {children}
-                </Modal>
-            </motion.section>
+            {modal.isOpen && (
+                <motion.section
+                    className="absolute top-0 flex bg-gray-950 bg-opacity-50 w-screen h-screen flex-col justify-center items-center"
+                    onClick={handleCloseModal}
+                    initial={{ opacity: 0, position: 'absolute', y: 100 }}
+                    animate={{ opacity: 1, position: 'absolute', y: 0 }}
+                    exit={{ opacity: 0, position: 'absolute', y: 100 }}
+                    transition={{ duration: 0.5 }}
+                >
+                    <Modal >
+                        {children}
+                    </Modal>
+                </motion.section>
+            )}
         </AnimatePresence>
     )
 }
