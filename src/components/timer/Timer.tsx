14b31ca5e5085d87e43c782c760312a2d2a883c6
jaime0506 <jaimemejia1003@gmail.com>
2023-10-22 22:35:35 -0500

    advances

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -22,16 +22,44 @@ const initialState: Timer = {
 
 export const Timer = () => {
     const [timer, setTimer] = useState<Timer>(initialState)
+    const [timerForm, setTimerForm] = useState({
+        minutes: initialState.minutes,
+        seconds: initialState.seconds
+    });
 
-    const changeTime = () => {
-        setTimer({...timer})
+    const increment = () => {
+        setTimer({ ...timer, ["minutes"]: timer.minutes + 1 })
     }
 
+    const decrement = () => {
+        setTimer({ ...timer, ["minutes"]: timer.minutes - 1 })
+    }
+
+    const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+        const newValue = e.target.value;
+
+        if (newValue.length <= 2) {
+            setTimerForm({
+                ...timerForm,
+                [e.target.name]: parseInt(e.target.value),
+            })
+        }
+    }
     // Correcion error de Ts al no usar la funcion ni el setTimer del useState
     return (
-        <div onClick={changeTime}>
-            <span className='font-clock'>
-                { timer.minutes }:{ timer.seconds > 9 ? timer.seconds : timer.seconds + "0"}
+        <div>
+            <span>
+                <input
+                    type='number'
+                    className='font-clock'
+                    onChange={onChangeInput}
+                    value={timerForm.minutes}
+                    name='minutes'
+                    maxLength={2}
+                />
+                <button className='border-black rounded-sm border-2' onClick={increment}>Arriba</button>
+                <button className='border-black rounded-sm border-2' onClick={decrement}>Abajo</button>
             </span>
         </div>
     )
