b2198f1cc0866aaa2d0d8b2efa107313ae3844d4
jaime0506 <jaimemejia1003@gmail.com>
2024-01-14 23:30:36 -0500

    Open modal when the pomodoro timer has ended, but I have a bug because, the modal that opens is from the father

--- src/components/pomodoro/PomodoroItem.tsx
+++ src/components/pomodoro/PomodoroItem.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 
 import { calculateTimeLeft, formatTime } from "../../helpers"
-import { usePomodoroStore } from "../../hooks"
+import { usePomodoroStore, useUiStore } from "../../hooks"
+import { TimerAlert } from "../timerAlert/TimerAlert"
 
 import alarm from '../../sounds/alarm.mp3'
 
@@ -9,8 +10,16 @@ import type { Timer } from "../../types"
 
 export const PomodoroItem = ({ uid, pomodoro: { minute, seconds } }: Timer) => {
 
-    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(minute, seconds))
     const { onDeleteTimer } = usePomodoroStore()
+
+    // No sirve porque al controlar la visibilidad del Modal desde un estado global, estamos
+    // modificando ese valor en los otros componentes que usen tambien el modal, dando prioridad
+    // a los de mayor jerarquia, debo idear lamanera de que reconozca desde donde lo estan llamando
+    // Para asi mismo no disparar los modales usados en componentes Padres.
+    
+    const { handleOpenModal } = useUiStore()
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(minute, seconds))
     const [audio] = useState(new Audio(alarm))
 
     const startSound = () => {
@@ -20,6 +29,7 @@ export const PomodoroItem = ({ uid, pomodoro: { minute, seconds } }: Timer) => {
     const handleOnDeleteTimer = () => {
         console.log("Se borra")
         onDeleteTimer(uid)
+        handleOpenModal()
     }
 
     useEffect(() => {
@@ -31,7 +41,7 @@ export const PomodoroItem = ({ uid, pomodoro: { minute, seconds } }: Timer) => {
                     return newTime
                 } else {
                     startSound()
-                    
+
                     clearInterval(interval)
                     handleOnDeleteTimer()
                     return 0
@@ -44,17 +54,35 @@ export const PomodoroItem = ({ uid, pomodoro: { minute, seconds } }: Timer) => {
     }, [])
 
     return (
-        <article className="w-[292px] rounded bg-white shadow-sm p-4 flex ">
-            <header className="flex-1">
-                <p className="text-lg">{formatTime(timeLeft)}</p>
-            </header>
+        <>
+            <TimerAlert />
+            
+            <article className="w-[292px] rounded bg-white shadow-sm p-4 flex ">
+                <header className="flex-1">
+                    <p className="text-lg">{formatTime(timeLeft)}</p>
+                </header>
 
-            <footer className="">
-                <i 
-                    className="fa-solid fa-circle-xmark text-xl hover:cursor-pointer text-red-500"
-                    onClick={handleOnDeleteTimer}
-                ></i>
-            </footer>
-        </article>
+                <footer className="">
+                    <i
+                        className="fa-solid fa-circle-xmark text-xl hover:cursor-pointer text-red-500"
+                        onClick={handleOnDeleteTimer}
+                    ></i>
+                </footer>
+            </article>
+        </>
     )
 }
+
+
+// <article className="w-[292px] rounded bg-white shadow-sm p-4 flex ">
+//             <header className="flex-1">
+//                 <p className="text-lg">{formatTime(timeLeft)}</p>
+//             </header>
+
+//             <footer className="">
+//                 <i
+//                     className="fa-solid fa-circle-xmark text-xl hover:cursor-pointer text-red-500"
+//                     onClick={handleOnDeleteTimer}
+//                 ></i>
+//             </footer>
+//         </article>
---
+++ src/components/timerAlert/TimerAlert.tsx
@@ -0,0 +1,9 @@
+import { ModalWrapper } from ".."
+
+export const TimerAlert = () => {
+  return (
+    <ModalWrapper>
+        <div>Se acabo el tiempo papu</div>
+    </ModalWrapper>
+  )
+}
